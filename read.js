663a781296bc6ce505872a19138d22f4efe898c1
adler3d <adler3d@gmail.com>
2020-08-06 21:39:53 +0300

    Update read.js

--- read.js
+++ read.js
@@ -1,10 +1,21 @@
+var qp={mode:"by_days",tail_k:"0"};process.argv.slice(2).map(e=>e.split("=")).map(e=>qp[e[0]]=e.length<2?"true":e[1]);
 var fs=require('fs');
 var getdef=(m,k,def)=>{if(!(k in m))m[k]=def;return m[k];};
 var qapsum=(arr,cb)=>{if(typeof cb=='undefined')cb=e=>e;return arr.reduce((pv,ex)=>pv+cb(ex),0);}
 var json=JSON.stringify;
 var mapkeys=Object.keys;var mapvals=(m)=>mapkeys(m).map(k=>m[k]);
 var rev=e=>e.trim().split(".").reverse().join(".");
-var str=(""+fs.readFileSync('nvme_main.log'));
+var tail=(fn,k)=>{
+  var f=fs.openSync(fn,'r');
+  var all=fs.statSync(f).size;
+  var pos=(k*all)|0;
+  var buf=Buffer.alloc(all-pos);
+  var n=fs.readSync(f,buf,pos,all-pos,null);
+  fs.closeSync(f);
+  return buf;
+}
+var fn='nvme_main.log';var k=parseFloat(qp.tail_k);
+var str=""+(k?tail(fn,k):fs.readFileSync(fn));
 var qqq=str;//.substr(str.indexOf(rev("2020.04.23")));
 var L=qqq.split("\r").join("").split("\n");
 var out=[];
@@ -19,8 +30,7 @@ L.map(e=>{
     var rec={d,t,rd_gb,wd_gb};getdef(d2i,d,[]).push(rec);out.push(rec);
   }
 });
-var argv=process.argv;
-if(argv.length==3&&argv[2]=="debug"){console.log(out.map(json).join("\n"));process.exit();}
+if(qp.mode==="by_recs"){console.log(out.map(json).join("\n"));process.exit();}
 mapkeys(d2i).map(d=>{
   var q=d2i[d];var f=v=>v.toFixed(3);
   d2i[d]={d,n:q.length,rd_gb:f(qapsum(q,e=>e.rd_gb)),wd_gb:f(qapsum(q,e=>e.wd_gb))}
